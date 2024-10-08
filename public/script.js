8550ab430545cc619f2ec926187e182f6b607ede
ug-devops <ug.khan@hotmail.com>
2024-10-08 15:40:15 +0500

    First commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // public/script.js
 document.getElementById('greetButton').addEventListener('click', function() {
-    const message = "Hello! Thank you for visiting my Elasticbeanstalk Node.js app!";
+    const message = "Hello! Thank you for visiting my ECS Fargate Node.js app!";
     document.getElementById('greetingMessage').textContent = message;
 });
 
