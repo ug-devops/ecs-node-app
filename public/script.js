f898b3585f97c2fc9e319436f1c5d20028f4e61e
root <root@DESKTOP-BUK3ROL.>
2024-11-07 11:01:09 +0500

    updated the docker image to v2

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // public/script.js
 document.getElementById('greetButton').addEventListener('click', function() {
-    const message = "Hello! Thank you for visiting my ECS Fargate Node.js app!";
+    const message = "Hello! Thank you for visiting my Kubernetes/ArgoCD Node.js app!";
     document.getElementById('greetingMessage').textContent = message;
 });
 
